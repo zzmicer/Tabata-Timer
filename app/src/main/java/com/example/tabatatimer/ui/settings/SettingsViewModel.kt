9823e7ba65624cf0ea3a62b114d52d550f8cdfff
Dmitry Shkadarevich <41323462+KastusKalinovski@users.noreply.github.com>
2020-12-16 18:54:23 +0300

    Add menu and gradient drawables

--- app/src/main/java/com/example/tabatatimer/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/com/example/tabatatimer/ui/settings/SettingsViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.tabatatimer.ui.notifications
+package com.example.tabatatimer.ui.settings
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class SettingsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is settings Fragment"
     }
     val text: LiveData<String> = _text
 }
