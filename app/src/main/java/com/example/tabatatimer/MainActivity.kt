81c5b6b22313c92740afc561dfabd5851938cb0a
Dmitry Shkadarevich <41323462+KastusKalinovski@users.noreply.github.com>
2020-12-21 14:42:57 +0300

    Add settings page via Android X preference

--- app/src/main/java/com/example/tabatatimer/MainActivity.kt
+++ app/src/main/java/com/example/tabatatimer/MainActivity.kt
@@ -3,13 +3,8 @@ package com.example.tabatatimer
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.example.tabatatimer.ui.dashboard.DashboardViewModel
-import com.example.tabatatimer.ui.home.HomeViewModel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,5 +14,7 @@ class MainActivity : AppCompatActivity() {
 
         val navController = findNavController(R.id.nav_host_fragment)
         navView.setupWithNavController(navController)
+
     }
+
 }
